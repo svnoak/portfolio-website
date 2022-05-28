8e8bee07ce8b0107c18b2a109aeef55de7615c3e
svnoak <kimpfimp@posteo.net>
2022-05-28 14:31:26 +0200

    adding navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+function Navigation() {
+    <nav>
+        <ul>
+            <li>Start</li>
+            <li>About me</li>
+            <li>Projects</li>
+            <li>Skills</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+}
+
+export default Navigation;
