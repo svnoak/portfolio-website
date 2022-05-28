ed5998fb127b47f4eccedfa8a585fab407953927
svnoak <kimpfimp@posteo.net>
2022-05-28 14:34:48 +0200

    adding welcome section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './assets/css/App.css';
+import Navigation from './components/Navigation';
+import Welcome from './components/Welcome';
 import ProjectList from './components/ProjectsList';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Welcome />
       <ProjectList />
     </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,18 @@
+import Navigation from "./Navigation";
+
 function Welcome() {
     return(
         <section id="welcome">
-
+            <div>
+                <Navigation />
+            </div>
+            <div className="introduction">
+                <img src="./assets/images/portrait.png" alt="Picture of Kim smiling"/>
+                <div className="portrait-description">
+                    <p>Hi, I'm Kim.</p>
+                    <p>Pleased to meet you.</p>
+                </div>
+            </div>
         </section>
     )
 }
